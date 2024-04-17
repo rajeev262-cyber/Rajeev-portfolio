ee15df32953ff76739d616428a0e3cbfd7ad6283
rajeev <rajeevyadav5758@gmial.com>
2024-04-17 15:42:44 +0530

    information update

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p,.about-content ', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Developer','Backend Developer','Web Developer'],
+    strings: ['Frontend Developer','Web Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
